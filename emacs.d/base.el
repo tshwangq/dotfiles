797b73f52d3042c4d3b483377ac4f22bdec75d69
Qun <tshwangq@gmail.com>
2017-10-31 13:57:08 +0800

    use emacs-china mirror

--- emacs.d/base.el
+++ emacs.d/base.el
@@ -14,14 +14,14 @@
 
 
 (package-initialize)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "http://elpa.emacs-china.org/org/") t)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/")
              '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
 ;                         ("marmalade" . "http://marmalade-repo.org/packages/")
                                         ;("melpa" . "https://melpa.org/packages/")
-                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")
                                         ;  ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
                          ))
 
